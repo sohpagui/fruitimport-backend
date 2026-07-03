040157920b5f7f34a39497a4b25a19defa80f1db
pascal237 <paguipascal6@gmail.com>
2026-07-03 17:37:45 +0100

    Fix Cloudinary credentials - use root key

--- dist/lib/cloudinary.js
+++ dist/lib/cloudinary.js
@@ -3,8 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const cloudinary_1 = require("cloudinary");
 cloudinary_1.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'dded2qk4f',
-    api_key: process.env.CLOUDINARY_API_KEY || '571565212462442',
-    api_secret: process.env.CLOUDINARY_API_SECRET || 'pq2UieH94wdOcaLAUR-ZUcxrRHM',
+    api_key: process.env.CLOUDINARY_API_KEY || '477372919553638',
+    api_secret: process.env.CLOUDINARY_API_SECRET || '7tBmaxp4cuTk7ILaNs9uqXQHvRQ',
+    secure: true,
 });
 exports.default = cloudinary_1.v2;
 //# sourceMappingURL=cloudinary.js.map
--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -2,8 +2,9 @@ import { v2 as cloudinary } from 'cloudinary'
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'dded2qk4f',
-  api_key: process.env.CLOUDINARY_API_KEY || '571565212462442',
-  api_secret: process.env.CLOUDINARY_API_SECRET || 'pq2UieH94wdOcaLAUR-ZUcxrRHM',
+  api_key: process.env.CLOUDINARY_API_KEY || '477372919553638',
+  api_secret: process.env.CLOUDINARY_API_SECRET || '7tBmaxp4cuTk7ILaNs9uqXQHvRQ',
+  secure: true,
 })
 
 export default cloudinary
