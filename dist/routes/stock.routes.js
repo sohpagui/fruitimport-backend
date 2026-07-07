fa1fa9b97c127c3f75d7e40cae88abb7aa97a57e
pascal237 <paguipascal6@gmail.com>
2026-07-07 23:42:51 +0100

    Add GET pertes endpoint

--- dist/routes/stock.routes.js
+++ dist/routes/stock.routes.js
@@ -1,4 +1,37 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const stock_controller_1 = require("../controllers/stock.controller");
@@ -14,5 +47,22 @@ router.get('/alertes', auth_middleware_1.authentifier, (0, auth_middleware_1.aut
 router.post('/reception', auth_middleware_1.authentifier, (0, auth_middleware_1.autoriser)(client_1.Role.MAGASINIER, client_1.Role.SECRETAIRE), stock_controller_1.receptionMarchandise);
 // Pertes (magasinier uniquement)
 router.post('/pertes', auth_middleware_1.authentifier, (0, auth_middleware_1.autoriser)(client_1.Role.MAGASINIER, client_1.Role.SECRETAIRE), stock_controller_1.perteStock);
+router.get('/pertes', auth_middleware_1.authentifier, (0, auth_middleware_1.autoriser)(client_1.Role.PDG, client_1.Role.SECRETAIRE, client_1.Role.MAGASINIER), async (req, res) => {
+    const prisma = (await Promise.resolve().then(() => __importStar(require('../lib/prisma')))).default;
+    const { repondreSucces, repondreErreur } = await Promise.resolve().then(() => __importStar(require('../utils/response')));
+    try {
+        const agenceId = req.query.agence_id ? parseInt(req.query.agence_id) : undefined;
+        const pertes = await prisma.perte.findMany({
+            where: agenceId ? { agenceId } : {},
+            include: { fruit: true, agence: true, calibre: true },
+            orderBy: { date: 'desc' },
+            take: 50
+        });
+        return repondreSucces(res, { pertes });
+    }
+    catch (e) {
+        return repondreErreur(res, e.message, 500);
+    }
+});
 exports.default = router;
 //# sourceMappingURL=stock.routes.js.map
--- src/routes/stock.routes.ts
+++ src/routes/stock.routes.ts
@@ -17,5 +17,19 @@ router.post('/reception', authentifier, autoriser(Role.MAGASINIER, Role.SECRETAI
 
 // Pertes (magasinier uniquement)
 router.post('/pertes', authentifier, autoriser(Role.MAGASINIER, Role.SECRETAIRE), perteStock)
+router.get('/pertes', authentifier, autoriser(Role.PDG, Role.SECRETAIRE, Role.MAGASINIER), async (req, res) => {
+  const prisma = (await import('../lib/prisma')).default
+  const { repondreSucces, repondreErreur } = await import('../utils/response')
+  try {
+    const agenceId = req.query.agence_id ? parseInt(req.query.agence_id as string) : undefined
+    const pertes = await prisma.perte.findMany({
+      where: agenceId ? { agenceId } : {},
+      include: { fruit: true, agence: true, calibre: true },
+      orderBy: { date: 'desc' },
+      take: 50
+    })
+    return repondreSucces(res, { pertes })
+  } catch (e: any) { return repondreErreur(res, e.message, 500) }
+})
 
 export default router
